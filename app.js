ec3ad60b6fbee87b4359297abba57587fada0c06
Koushik <Koushikbajpayee06@gmail.com>
2025-11-11 18:34:04 +0530

    explaiation about jsx

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1",{id:"heading"},"This is react from cdn");
-console.log(heading)
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
 
-//JSX
-    const jsxHeading = <h1 id="heading">This is heading using 🚀</h1>
-    console.log(jsxHeading)
+// JSX =>Babel Transpiles to  React.createElement => ReactElement-JS Object - HTMLElement(render)
+
+const jsxHeading = (<h1 id="heading" className="head">
+    This is heading using 🚀
+    </h1>
+    ;)
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
