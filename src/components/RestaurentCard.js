175ba9e834a2c4404b5a8f65e9c3edfa399364e2
Koushik <Koushikbajpayee06@gmail.com>
2025-11-13 22:53:28 +0530

    props passing - destructuring on the fly

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
-const RestaurentCard = (props) => {
+const RestaurentCard = ({resName,cuisine,ratings,deliveryTime}) => {
+    // console.log(props);
   return (
     <div className='res-card'>
         <img className="res-img" alt='res-logo' src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/10/17/641102cd-be56-4c4b-9df1-a89fcc4430ad_39309.JPG'/>
         <div className='res-info'>
-            <h3 className="res-name">{props.resName}</h3>
-             <p className="res-cuisines">{props.cuisine}</p>
+            <h3 className="res-name">{resName}</h3>
+             <p className="res-cuisines">{cuisine}</p>
              <div className="res-details">
-                <span className="res-rating">⭐ {props.ratings}</span>
+                <span className="res-rating">⭐ {ratings}</span>
                 <span>•</span>
-                <span>{props.deliveryTime} mins</span>
+                <span>{deliveryTime} mins</span>
              </div>
         </div>
 
