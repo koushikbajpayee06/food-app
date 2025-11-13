75fc236954fb55fdf2a9ee54817e465d144d7c69
Koushik <Koushikbajpayee06@gmail.com>
2025-11-13 22:49:17 +0530

    Passing props and make card dynamic

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,18 +6,8 @@ const Body = () => {
     <div className='body'>
       <div className='search'></div>
       <div className='res-container'>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
-        <RestaurentCard/>
+        <RestaurentCard resName="KFC" cuisine="Burgers, Fast Food, Rolls & Wraps" ratings="4.6" deliveryTime="30"/>
+        <RestaurentCard resName="Dominos" cuisine="Pizza" ratings="4.5"  deliveryTime="24"/>
       </div>
     </div>
   )
--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const RestaurentCard = () => {
+const RestaurentCard = (props) => {
   return (
     <div className='res-card'>
         <img className="res-img" alt='res-logo' src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/10/17/641102cd-be56-4c4b-9df1-a89fcc4430ad_39309.JPG'/>
         <div className='res-info'>
-            <h3 className="res-name">KFC</h3>
-             <p className="res-cuisines">Burgers, Fast Food, Rolls & Wraps</p>
+            <h3 className="res-name">{props.resName}</h3>
+             <p className="res-cuisines">{props.cuisine}</p>
              <div className="res-details">
-                <span className="res-rating">⭐ 4.5</span>
+                <span className="res-rating">⭐ {props.ratings}</span>
                 <span>•</span>
-                <span>38 mins</span>
+                <span>{props.deliveryTime} mins</span>
              </div>
         </div>
 
