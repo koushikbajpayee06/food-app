0dcf0b9981b8c72a03fb9d6a191bd435a4cd7324
Koushik <Koushikbajpayee06@gmail.com>
2025-11-19 17:50:45 +0530

    Search button implemented

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import RestaurentCard from './RestaurentCard'
-import ShimmerUI from './shimmerUI';
+import React, { useEffect, useState } from "react";
+import RestaurentCard from "./RestaurentCard";
+import ShimmerUI from "./shimmerUI";
 
 // restaurantList.js
 
@@ -13,45 +13,71 @@ const extractImageId = (id) => {
 };
 
 const Body = () => {
-  const [listOfRestaurents,setListOfRestaurents] = useState([]);
+  const [listOfRestaurents, setListOfRestaurents] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  console.log("body rendered");
 
-  useEffect(()=>{
-    fetchData()
-  },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const fetchData = async()=>{
+  const fetchData = async () => {
     const data = await fetch("https://namastedev.com/api/v1/listRestaurants");
-    const json = await data.json()
-    // console.log(json.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-    setListOfRestaurents(json?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }
+    const json = await data.json();
+    console.log(
+      json.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setListOfRestaurents(
+      json?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
 
-  if(listOfRestaurents.length===0){
-    return <ShimmerUI/>
-  }
-  
-  return (
-    <div className='body'>
-      <div className='filter'>
-        <button className='filter-btn'
-         onClick={
-          ()=>{
+  return listOfRestaurents.length === 0 ? (
+    <ShimmerUI />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search ">
+          <input
+            type="text"
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            className="search-box"
+            value={searchText}
+          />
+          <button
+            onClick={() => {
+              // console.log(searchText);
+              const filteredRestaurent = listOfRestaurents.filter(
+                (res) => res.info.name.includes(searchText) 
+              );
+              setListOfRestaurents(filteredRestaurent);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
             const filterList = listOfRestaurents.filter(
-            (item)=>item.info.avgRating>4.5)
+              (item) => item.info.avgRating > 4.5
+            );
             setListOfRestaurents(filterList);
-          }
-        }     
-         >Top Rated Resaurents</button>
+          }}
+        >
+          Top Rated Resaurents
+        </button>
       </div>
-      <div className='res-container'>
-        {
-          listOfRestaurents.map((res)=><RestaurentCard 
-          key={res.info.id} resData={res.info}/>)
-        }
-        
+      <div className="res-container">
+        {listOfRestaurents.map((res) => (
+          <RestaurentCard key={res.info.id} resData={res.info} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import { Logo_URL } from '../utills/constant'
 
 const Header = () => {
-  const [btnName, setBtnName] = useState('Login')
+  const [btnName, setBtnName] = useState('Login');
+  console.log("Header Render");
+
   return (
     <div className='header'>
       <div className='logo-container'>
