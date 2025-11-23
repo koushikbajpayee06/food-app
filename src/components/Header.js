7674e6aafe78e51e90bdc53faeada0a67ff18336
Koushik <Koushikbajpayee06@gmail.com>
2025-11-23 22:04:50 +0530

    using tailwind css class

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,27 +10,29 @@ const Header = () => {
   const OnlineStatus = useOnlineStatus();
 
   return (
-    <div className='header'>
+    <div className='flex justify-between bg-pink-500'>
       <div className='logo-container'>
-        <img className='logo' src={Logo_URL}/>
+        <img className='w-28' src={Logo_URL}/>
       </div>
       <div className='nav-items'>
-        <ul>
+        <ul className='flex p-4 m-4'>
             <li>
               Online Status:{OnlineStatus?"✅":"🔴"}
             </li>
             <li>
               <Link to='/'>Home</Link></li>
             <li>
-              <Link to='/about'>About Us</Link></li>
+              <Link to='/about'>About Us</Link>
+            </li>
             <li>
               <Link to='/contact'>Contact Us</Link>
-              </li>
+            </li>
             <li>
               <Link to='/grocery'>Grocery</Link>
             </li>
             <li>
-              <Link to='/cart'>Cart</Link></li>
+              <Link to='/cart'>Cart</Link>
+            </li>
           <button className='login' onClick={()=>{
             btnName==="Login"?
              setBtnName("Logout"):
