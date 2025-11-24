3cb45981fadeceb6b717c4d3b04ea729899708cb
Koushik <Koushikbajpayee06@gmail.com>
2025-11-25 00:23:15 +0530

    lifting the stateup compleated

--- src/components/RestaurentMenu.js
+++ src/components/RestaurentMenu.js
@@ -6,7 +6,6 @@ import useMenu from '../utills/useMenu';
 import useAllMenu from '../utills/useAllMenu';
 import RestaurentCategory from './RestaurentCategory';
 
-
 const RestaurentMenu = () => {
 
   const {resId} = useParams();
@@ -15,6 +14,7 @@ const RestaurentMenu = () => {
     const resInfo = useRestaurentMenu(resId);
   
     const catagories = useAllMenu(resId);
+    const [showIndex, setShowIndex] = useState(0)
   
 
   if(resInfo === null || catagories === null) return <ShimmerUI/>;
@@ -31,9 +31,13 @@ const RestaurentMenu = () => {
       <h1 className="text-3xl  font-bold my-6">{name}</h1>
       <p className='font-bold'>{cuisines?.join(', ')} - {costForTwo}</p>
       {/* categories accordians */}
-      {catagories.map((category)=>
-        <RestaurentCategory key={category.card.card.title} 
-        data={category.card.card} showItems={false}/>
+      {catagories.map((category,index)=>
+        <RestaurentCategory 
+        key={category.card.card.title} 
+        data={category.card.card} 
+        showItems={index===showIndex ? true: false}
+        setShowIndex={()=>setShowIndex(index)}
+        />
         
       )}
     </div>
