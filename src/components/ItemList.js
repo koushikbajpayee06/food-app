71b2deea088ee4396d11a4cca5ca9e5669a88c1a
Koushik <Koushikbajpayee06@gmail.com>
2025-11-24 17:48:51 +0530

    accordian added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,4 +1,5 @@
-import { CDN_URL } from "../utills/constant";
+import { BASE, CDN_URL } from "../utills/constant";
+BASE
 const ItemList = ({items}) =>{
     console.log(items);
     return ( 
@@ -7,18 +8,27 @@ const ItemList = ({items}) =>{
                 items.map(item=>(
                 <div 
                     key={item.card.info.id } 
-                    className="p-2 m-2 border-gray-200 border-b-2 text-left"
-                >
-                    <img src={CDN_URL+ item.card.info.imageId} className="w-14"></img>
-                    <div className="p-2">
-                        <span>{item.card.info.name}</span>
-                        <span>
-                            {" "} 
-                            - ₹{" "}
-                            {item.card.info.price/100}
-                        </span>
+                    className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between">
+                    <div className="w-9/12">
+                        <div className="p-2">
+                            <span>{item.card.info.name}</span>
+                            <span>
+                                {" "} 
+                                - ₹{" "}
+                                {item.card.info.price/100}
+                            </span>
+                        </div>
+                        <p className="text-xs">{item.card.info.description}</p>
                     </div>
-                    <p className="text-xs">{item.card.info.description}</p>
+                    <div className="w-3/12 p-4">
+                        <div className="absolute">
+                            <button className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg">
+                                Add +
+                            </button>
+                        </div>
+                        <img src={CDN_URL+ item.card.info.imageId} className="w-full"></img>
+                    </div>
+                    
                 </div>
                 
             ))}
--- src/components/RestaurentCategory.js
+++ src/components/RestaurentCategory.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ItemList from './ItemList'
 
 const RestaurentCategory = ({data}) => {
+
+  const [showItems, setShowItems] = useState(false)
+
+  const handleClick = ()=>{
+    setShowItems(!showItems)
+  }
     // console.log(data)
   return (
     <div>
       {/** Header **/}
         <div className='w-6/12 mx-auto my-4  bg-gray-50 shadow-lg p-4'>
-            <div className='flex justify-between'>
+            <div className='flex justify-between cursor-pointer' onClick={handleClick}>
                 <span className='font-bold text-lg'>
                     {data.title}  ({data.itemCards.length})
                 </span>
                 <span>⬇️</span>
             </div>
-            <ItemList items = {data.itemCards}/>
+            {showItems && <ItemList items = {data.itemCards}/>}
         </div>
       
     </div>
