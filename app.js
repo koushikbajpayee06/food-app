63e13d3573bbf9a62c040e3b009aa01a22771c58
Koushik <Koushikbajpayee06@gmail.com>
2025-11-26 10:51:23 +0530

    store is provided to our app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import React, {lazy,Suspense, useEffect, useState} from "react";
-import ReactDOM from "react-dom/client"
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
@@ -9,70 +9,77 @@ import Error from "./src/components/Error";
 import Cart from "./src/components/Cart";
 import RestaurentMenu from "./src/components/RestaurentMenu";
 import UserContext from "./src/utills/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./src/utills/appStore";
 // import Grocery from "./src/components/Grocery";
 
-const AboutUs = lazy(()=>import("./src/components/AboutUs"))
+const AboutUs = lazy(() => import("./src/components/AboutUs"));
 
+const Grocery = lazy(() => import("./src/components/Grocery"));
 
-const Grocery = lazy(()=>import("./src/components/Grocery"));
+const AppLayout = () => {
+  // authintication
+  const [userName, setUserName] = useState();
 
-const AppLayout = ()=>{
-    // authintication 
-    const [userName, setUserName] = useState();
-
-    useEffect(()=>{
-        const data = {
-            name:"Koushik Bajpayee"
-        };
-        setUserName(data.name);
-    },[])
-    return(
-        <UserContext.Provider value={{loggedInUser:userName, setUserName }}>
-            <div className="app">
-                <Header />
-                <Outlet />
-            </div>
-        </UserContext.Provider>
-    );
+  useEffect(() => {
+    const data = {
+      name: "Koushik Bajpayee",
+    };
+    setUserName(data.name);
+  }, []);
+  return (
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
+  );
 };
 
 const appRouter = createBrowserRouter([
-    {
-        path:'/',
-        element:<AppLayout/>,
-        children:[
-            {
-                path:'/',
-                element:<Body/>
-            },
-            {
-                path:'/about',
-                element:<Suspense fallback={<h1>Loading2...</h1>}><AboutUs/></Suspense>
-            },
-            {
-                path:'/contact',
-                element:<Contact/>
-            },
-            {
-                path:'/grocery',
-                element:
-                    <Suspense 
-                        fallback={<h1>Loading ....</h1>}><Grocery/>
-                    </Suspense> 
-            },
-            {
-                path:'/cart',
-                element:<Cart/>
-            },
-            {
-                path:'/restaurant/:resId',
-                element:<RestaurentMenu/>
-            },
-        ],
-        errorElement:<Error/>
-    }
-    
-])
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading2...</h1>}>
+            <AboutUs />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading ....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurentMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={appRouter}/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
