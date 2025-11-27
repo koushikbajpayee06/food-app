249035d6e0103f96e59f03b34a77aeea46ff9685
Koushik <Koushikbajpayee06@gmail.com>
2025-11-27 13:48:15 +0530

    Unit testing of a component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h2>This is Contact us page</h2>
+      <h2 className='font-bold text-2xl p-4 m-4 text-center'>This is Contact Us page</h2>
+      <form>
+        <input type='text' className='border border-black p-2 m-2' placeholder='name'></input>
+        <input type='text' className='border border-black p-2 m-2' placeholder='message'></input>
+        <button className='border bg-gray-300 rounded-lg border-black p-2 m-2'>Submit</button>
+      </form>
     </div>
   )
 }
