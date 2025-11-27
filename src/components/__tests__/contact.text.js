cac717a9d6806df2947a8dcdd7b508fa76244d1d
Koushik <Koushikbajpayee06@gmail.com>
2025-11-27 14:53:19 +0530

    adding test cases

--- src/components/__tests__/contact.text.js
+++ src/components/__tests__/contact.text.js
@@ -4,9 +4,18 @@ import "@testing-library/jest-dom"
 
 test ("Should load contact us component",()=>{
     render(<Contact/>);
-
     const heading = screen.getByRole("heading");
-
     // Assertion
     expect(heading).toBeInTheDocument();
-})
+});
+test("should load Button inside my Contact Component",()=>{
+    render(<Contact/>)
+    // const button = screen.getByRole('button');
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+});
+test("search input is present inside contuct component", ()=>{
+    render(<Contact/>);
+    const input = screen.getByPlaceholderText('name');
+    expect(input).toBeInTheDocument();
+});
